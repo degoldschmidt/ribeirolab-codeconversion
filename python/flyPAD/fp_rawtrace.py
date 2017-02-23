1b73c2f87d92ce696ad993ac1d3376efef4428ed
degoldschmidt <goldschmidt.de@gmail.com>
2017-02-23 09:26:39 +0000

    Adjustments to rawtrace for MacOS

--- python/flyPAD/fp_rawtrace.py
+++ python/flyPAD/fp_rawtrace.py
@@ -32,6 +32,7 @@ import scipy as sp
 import scipy.signal as sg
 from scipy.signal import hilbert
 from itertools import groupby
+import platform
 
 # metadata
 __author__                  = "Dennis Goldschmidt"
@@ -182,6 +183,25 @@ def main(argv):
     # if no files are given
     if len(files) == 0:
         print("WARNING: No valid files specified.")
+"""
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+import numpy as np
+plt = pg.plot(np.random.normal(size=100), title="Simplest possible plotting example")
+plt.getAxis('bottom').setTicks([[(x*20, str(x*20)) for x in range(6)]])
+## Start Qt event loop unless running in interactive mode or using pyside.
+#ex = pg.exporters.SVGExporter.SVGExporter(plt.plotItem.scene())
+#ex.export('/home/luke/tmp/test.svg')
+
+if __name__ == '__main__':
+    import sys
+    if sys.flags.interactive != 1 or not hasattr(QtCore, 'PYQT_VERSION'):
+        pg.QtGui.QApplication.exec_()
+
+"""
 
 if __name__ == "__main__":
+    if "Darwin" in platform.platform():
+        print("Is a Mac.")
+        quit()
     main(sys.argv[1:])
