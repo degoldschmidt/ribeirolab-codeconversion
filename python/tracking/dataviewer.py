4ab075d3244a8bd6308b4e3db9f8e4d790c5d43a
degoldschmidt <goldschmidt.de@gmail.com>
2017-06-28 00:16:06 +0100

    json transfer works

--- python/tracking/dataviewer.py
+++ python/tracking/dataviewer.py
@@ -5,6 +5,8 @@ from tkinter import *
 from tkinter import messagebox, filedialog
 from tkinter import ttk
 import datetime as dt
+import os
+import codecs,json
 
 def read_csv(_file, delimiter=';'):
     with open(_file,'r') as dest_f:
@@ -17,27 +19,48 @@ def read_csv(_file, delimiter=';'):
 
 if __name__ == "__main__":
     Tk().withdraw()
-    _file = "E:/Dennis/Google Drive/PhD Project/Archive/VERO/tracking_data/CANS_Info_fly_001.csv" #filedialog.askopenfilename(title='Choose file to load')
+    if os.name == 'nt':
+        _file = "E:/Dennis/Google Drive/PhD Project/Archive/VERO/tracking_data/CANS_Info_fly_001.csv"
+    else:
+        #_files = filedialog.askopenfilenames(title='Choose file to load')
+        _files = filedialog.askdirectory(title='Choose files to load')
     #print(_file)
-    data = read_csv(_file, delimiter=';')
     #data = np.genfromtxt(_file, delimiter=';')
 
-    outdict = {}
-    for i,entry in enumerate(data[0]):
-        if i < len(data[0])-1:
-            print(i)
-            if data[0][i+1] != "" and entry != "":
-                if len(data[1][i]) > 1:
-                    outdict[str(entry)] = str(data[1][i])
-                else:
-                    outdict[str(entry)] = int(data[1][i])
-            elif data[0][i+1] == "" and data[0][i+1] != "Date" and entry != "Date":
-                array = []
-                print(i, entry)
-                for row in data[1:]:
-                    if len(row) > 0:
-                        array.append([float(row[i]) , float(row[i+1])])
-                outdict[str(entry)] = np.array(array)
-            elif entry == "Date":
-                outdict[str(entry)+"time"] = dt.datetime.strptime(data[1][i], "%d-%b-%Y %H:%M:%S")
-    print(outdict)
+    outdict = [{},{},{}]
+    for _file in os.listdir(_files):
+        exp = ["CANS", "ORCO", "TBEH"]
+        for idx,ex in enumerate(exp):
+            filedict = {}
+            if ex+"_Info" in _file and ".csv" in _file:
+                #print(_file)
+                data = read_csv(_files+os.sep+_file, delimiter=';')
+                for i,entry in enumerate(data[0]):
+                    if entry == "":
+                        pass
+                    elif entry == "PatchPositions":
+                        array = []
+                        for row in data[1:]:
+                            if len(row) > 0:
+                                array.append([float(row[i]) , float(row[i+1])])
+                        filedict[str(entry)] = array
+                    elif entry == "Date":
+                        filedict[str(entry)+"time"] = str(data[1][i])
+                    elif entry == "SubstrateType":
+                        array = []
+                        for row in data[1:]:
+                            if len(row) > 0:
+                                array.append(int(row[i]))
+                        filedict[str(entry)] = array
+                    else:
+                        if len(data[1][i]) > 1:
+                            filedict[str(entry)] = str(data[1][i])
+                        else:
+                            filedict[str(entry)] = int(data[1][i])
+                    splfil = _file.split("_")
+                    filedict["Datafile"] = splfil[0] + "_" + splfil[3]
+                    outdict[idx][splfil[0] + "_" + splfil[3].split(".")[0]] = filedict
+    exp = ["CANS", "ORCO", "TBEH"]
+    for idx,ex in enumerate(exp):
+        print(outdict[idx].keys())
+        json.dump(outdict[idx],codecs.open(_files+"/new_files/"+ex+".json", 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
