6260a24daa9b1da8dfc2d56f1d9261518c05f214
degoldschmidt <goldschmidt.de@gmail.com>
2017-06-28 09:03:01 +0100

    functionized json dump

--- python/tracking/dataviewer.py
+++ python/tracking/dataviewer.py
@@ -16,20 +16,10 @@ def read_csv(_file, delimiter=';'):
         data = [data for data in data_iter]
     return data
 
-
-if __name__ == "__main__":
-    Tk().withdraw()
-    if os.name == 'nt':
-        _file = "E:/Dennis/Google Drive/PhD Project/Archive/VERO/tracking_data/CANS_Info_fly_001.csv"
-    else:
-        #_files = filedialog.askopenfilenames(title='Choose file to load')
-        _files = filedialog.askdirectory(title='Choose files to load')
-    #print(_file)
-    #data = np.genfromtxt(_file, delimiter=';')
-
+def json_dump(_files, labels=["CANS", "ORCO", "TBEH"]):
     outdict = [{},{},{}]
+    exp = labels
     for _file in os.listdir(_files):
-        exp = ["CANS", "ORCO", "TBEH"]
         for idx,ex in enumerate(exp):
             filedict = {}
             if ex+"_Info" in _file and ".csv" in _file:
@@ -60,7 +50,19 @@ if __name__ == "__main__":
                     splfil = _file.split("_")
                     filedict["Datafile"] = splfil[0] + "_" + splfil[3]
                     outdict[idx][splfil[0] + "_" + splfil[3].split(".")[0]] = filedict
-    exp = ["CANS", "ORCO", "TBEH"]
     for idx,ex in enumerate(exp):
         print(outdict[idx].keys())
         json.dump(outdict[idx],codecs.open(_files+"/new_files/"+ex+".json", 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+
+
+if __name__ == "__main__":
+    Tk().withdraw()
+    if os.name == 'nt':
+        _files = "E:/Dennis/Google Drive/PhD Project/Archive/VERO/tracking_data/"
+    else:
+        #_files = filedialog.askopenfilenames(title='Choose file to load')
+        _files = filedialog.askdirectory(title='Choose files to load')
+    json_dump(_files)
+    ### READ OUT CSV HEAD and BODY CENTROID and BUILD DATAFILE PER SESSION  
+    #print(_file)
+    #data = np.genfromtxt(_file, delimiter=';')
