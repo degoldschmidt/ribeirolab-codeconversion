6477c7678447c627a7c4b57cf3df97a3d377caa8
degoldschmidt <goldschmidt.de@gmail.com>
2017-06-29 14:02:52 +0100

    New Appp

--- python/tracking/dataviewer.py
+++ python/tracking/dataviewer.py
@@ -1,10 +1,18 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox, filedialog
 from tkinter import ttk
 import data_integrity as di
 
+class DataViewerApp():
+    def __init__(self, master):
+        self.master = master
+        self.master.wm_title("DataViewer")
+
 if __name__ == "__main__":
-        Tk().withdraw()
-        _dir = filedialog.askdirectory(title='Choose database to load')
-        flags = di.data_check(_dir)
+        #Tk().withdraw()
+        #_dir = filedialog.askdirectory(title='Choose database to load')
+        #flags = di.data_check(_dir)
+        root = tk.Tk()
+        app = DataViewerApp(root)
+        root.mainloop()
         #print(flags)
