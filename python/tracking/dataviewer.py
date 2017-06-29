59b011a116133a31d90104bfceafb15b0f2dcadb
degoldschmidt <goldschmidt.de@gmail.com>
2017-06-29 18:38:48 +0100

    new app and gui elements

--- python/tracking/dataviewer.py
+++ python/tracking/dataviewer.py
@@ -2,11 +2,32 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 from tkinter import ttk
 import data_integrity as di
+from gui_elements import TreeListBox, MenuBar
 
 class DataViewerApp():
     def __init__(self, master):
         self.master = master
         self.master.wm_title("DataViewer")
+        menubar = MenuBar(self.master)
+        self.master.config(menu=menubar)
+
+        self.lframe = tk.LabelFrame(self.master, text = "Database structure")
+        self.lframe.grid(row=0, column=0, columnspan=2, rowspan=2)
+        self.tree = TreeListBox(self.lframe, "", {})
+
+        rtframe = tk.LabelFrame(self.master, text = "Parameters Info")
+        rtframe.grid(row=0, column=2)
+
+        rbframe = tk.LabelFrame(self.master, text = "Preview")
+        rbframe.grid(row=1, column=2)
+
+        self.info = tk.StringVar()
+        self.infotext = tk.Label(rtopframe, textvariable = self.info, relief = tk.SUNKEN, justify=tk.LEFT)
+        self.infotext.pack()
+
+    def update_tree(self, _title, _dict):
+        self.tree = TreeListBox(self.lframe, _title, _dict)
+
 
 if __name__ == "__main__":
         #Tk().withdraw()
