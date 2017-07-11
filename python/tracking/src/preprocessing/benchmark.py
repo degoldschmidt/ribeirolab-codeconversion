cbdb2a9692c27defe2f862b79fbe3335082c8d0d
degoldschmidt <goldschmidt.de@gmail.com>
2017-07-11 08:27:23 +0100

    benchmark more

--- python/tracking/src/preprocessing/benchmark.py
+++ python/tracking/src/preprocessing/benchmark.py
@@ -19,7 +19,7 @@ class benchmark(object):
 
 class multibench(object):
 
-    def __init__(self, times, msg, fmt="%0.9g", _SILENT=True):
+    def __init__(self, times, msg="", fmt="%0.9g", _SILENT=True):
         if not _SILENT:
             print("Start benchmark")
             self.msg = msg
@@ -28,10 +28,11 @@ class multibench(object):
             self.t = np.zeros(times)
 
     def __call__(self, f):
+        self.f = f
         for i,thistime in enumerate(self.t):
             with benchmark(self.msg) as result:
-                f()
+                self.f()
             self.t[i] = result.time
 
     def __del__(self):
-        print("Test completed. Total time: {:} s. Avg: {:} s. Max: {:} s.".format(np.sum(self.t), np.mean(self.t), np.max(self.t)))
+        print("Test {:} for {:} repititions. Total time: {:} s. Avg: {:} s. Max: {:} s.".format(self.f.__name__, len(self.t), np.sum(self.t), np.mean(self.t), np.max(self.t)))
--- python/tracking/src/preprocessing/gaussian_filter.py
+++ python/tracking/src/preprocessing/gaussian_filter.py
@@ -1,6 +1,7 @@
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import time
 from benchmark import benchmark, multibench
 
@@ -10,13 +11,29 @@ def gaussian_filtered(_X, _len=16, _sigma=1.6):
     window = signal.gaussian(_len+1, std=_sigma)/norm
     return np.convolve(_X, window, "same")
 
-def gaussian_test():
+testsamples = 1000000
+
+def gaussian_test_pd():
     #np.random.seed(42)
-    N=500000
+    N=testsamples
+    s = pd.Series(np.random.randn(N) + np.sin(np.linspace(0,50,num=N)))
+    convolved = gaussian_filtered(s, _len=16)
+
+def gaussian_test_np():
+    #np.random.seed(42)
+    N=testsamples
     noise = np.random.randn(N)
     series = np.sin(np.linspace(0,50,num=N))
     convolved = gaussian_filtered(series+noise, _len=16)
 
-test = multibench(100, "")
-test(gaussian_test)
-del test
+
+
+if __name__ == "__main__":
+    test = multibench(100)   # start benchmark for 10 repetitions
+    test(gaussian_test_np)  # perform benchmark for given function
+    del test                # delete benchmark for stats printout (test.t)
+    """
+    test2 = multibench(20)   # start benchmark for 10 repetitions
+    test2(gaussian_test_pd)  # perform benchmark for given function
+    del test2                # delete benchmark for stats printout (test.t)
+    """
